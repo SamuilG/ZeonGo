e9f9132dfdc8a97c3a8ac8c701927e4c8184ca00
SamuilG <samuil.georgiev@outlook.com>
2021-11-08 20:35:22 +0200

    ffffffffffffffffff-viewport...ilko

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.auth')
 
+@section('title')
+    Login
+@endsection
+
 @section('form')
-    <div class="card bg-light">
-        <div class="card-body" style="text-align: center">
+        <div style="text-align: center">
             @if (session('status'))
                 <div class="text-danger">
                     <h5>{{ session('status') }}</h5>
@@ -13,7 +16,7 @@
                 @csrf
                 {{-- Email --}}
                 <div class="form-group">
-                    <label for="email" style="float: left">Email address</label>
+                    <label for="email" class="text-white" style="float: left">Email address</label>
                     <input type="email" class="form-control @error('email') border border-danger @enderror" id="email" name="email" placeholder="Enter email" value="{{ old('email') }}">
                     @error('email')
                         <div class="text-danger"> {{$message}} </div>
@@ -21,7 +24,7 @@
                 </div>
         
                 <div class="form-group">
-                    <label for="password" style="float: left">Password</label>
+                    <label for="password" class="text-white" style="float: left">Password</label>
                     <input type="password" class="form-control @error('password') border border-danger @enderror" id="password" name="password" placeholder="Password">
                     @error('password')
                         <div class="text-danger"> {{$message}} </div>
@@ -30,8 +33,7 @@
                 <br>
                 <button type="submit" class="btn btn-primary col-8">Log in</button>
             </form>
-        </div>
-    </div>
+        </div>  
 
 
 
