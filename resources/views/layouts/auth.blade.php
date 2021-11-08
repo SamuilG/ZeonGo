852a21dda27af2a435960cb3efe09a50ca428a7c
SamuilG <samuil.georgiev@outlook.com>
2021-11-08 19:01:16 +0200

    login soon to be an idea

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 
+@section('headContent')
+    <link rel="stylesheet" href="/css/auth.css">
+@endsection
+
 @section('content')
-    <div class="container-fluid object-fit" >
-        <div class="row align-items-center">
-            <div class="col-4">
-                @yield('left-side')
-            </div>
-            <div class="col-8">
-                @yield('right-side')
+    <div id="bg-img">
+        <div class="container-fluid object-fit" >
+            <div class="row align-items-center">
+                <div class="col-4">
+                    @yield('left-side')
+                </div>
+                <div class="col-8">
+                    @yield('right-side')
+                </div>
             </div>
         </div>
-    </div>
+    </div>    
 @endsection
 
