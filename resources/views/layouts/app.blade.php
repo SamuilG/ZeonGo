13dea5729a5c5a19215d7c7df902b48fc86fb18e
SamuilG <samuil.georgiev@outlook.com>
2021-11-08 18:18:05 +0200

    holy shit laravel works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
+    <link rel="icon" type="image/png" href="/images/ZeonGoIcon.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark vh-50" style="height: 69px;"">
+      <a class="ms-3 h-100 navbar-brand" href="/">
+        <img class="h-100 img-fluid" src="/images/ZeonGoLogo.png" alt="ZeonGo">
+      </a>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          
+        
+          @auth
+              <li>
+                {{-- <a href="{{ route('home') }}" class="p-3">{{ auth()->user()->name  }}</a> --}}
+                <a href="#" class="p-3">Actial Name</a>
+              </li> 
+              <li>
+                {{-- <form action="{{ route('logout') }}" method="POST" class="p-3 inline"> --}}
+                <form action="#" method="POST" class="p-3 inline">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+              </li>
+          @endauth
+
+          @guest
+              <li>
+                {{-- <a href="{{ route('login') }}" class="p-3">Login</a> --}}
+                <a href="#" class="p-3">Login</a>
+              </li>  
+              <li>
+                <a href="{{ route('register') }}" class="p-3">Register</a>
+                {{-- <a href="#" class="p-3">Register</a> --}}
+              </li>
+          @endguest  
+
+        </ul>    
+      </div>
+    </nav>
 
 
-      @yield('content')
+    @yield('content')
 
-</body>
+  </body>
 </html>
