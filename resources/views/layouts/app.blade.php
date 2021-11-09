2357511d2c6ddd66b81960753f1776a0992215e0
SamuilG <samuil.georgiev@outlook.com>
2021-11-09 22:53:57 +0200

    fixed overflow
    also removed some useless divs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,44 +12,47 @@
     <title>@yield('title')</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand navbar-light bg-dark vh-50" style="height: 69px;">
-      <a class="ms-3 h-100 navbar-brand" href="/">
-        <img class="mh-100 img-fluid" src="/images/ZeonGoLogo.png" alt="ZeonGo">
-      </a>
+    <div id="wrapper">
+
     
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav" style="margin-left: auto">
+      <nav class="navbar navbar-expand navbar-light bg-dark vh-50" style="height: 69px;">
+        <a class="ms-3 h-100 navbar-brand" href="/">
+          <img class="mh-100 img-fluid" src="/images/ZeonGoLogo.png" alt="ZeonGo">
+        </a>
+      
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav" style="margin-left: auto">
+            
           
-        
-          @auth
-              <li>
-                <a href="{{ route('home') }}" class="p-3">{{ auth()->user()->name  }}</a>
-                {{-- <a href="#" class="p-3">Actial Name</a> --}}
-              </li> 
-              <li>
-                {{-- <form action="{{ route('logout') }}" method="POST" class="p-3 inline"> --}}
-                <form action="#" method="POST" class="p-3 inline">
-                    @csrf
-                    <button type="submit">Logout</button>
-                </form>
-              </li>
-          @endauth
+            @auth
+                <li>
+                  <a href="{{ route('home') }}" class="p-3">{{ auth()->user()->name  }}</a>
+                  {{-- <a href="#" class="p-3">Actial Name</a> --}}
+                </li> 
+                <li>
+                  {{-- <form action="{{ route('logout') }}" method="POST" class="p-3 inline"> --}}
+                  <form action="#" method="POST" class="p-3 inline">
+                      @csrf
+                      <button type="submit">Logout</button>
+                  </form>
+                </li>
+            @endauth
 
-          @guest
-              <li>
-                <a href="{{ route('login') }}" class="me-2 btn btn-outline-light" role="button" aria-disabled="true">Login</a>
-              </li>  
-              <li>
-                <a href="{{ route('register') }}" class="me-3 btn btn-outline-light" role="button" aria-disabled="true">Register</a>
-              </li>
-          @endguest  
+            @guest
+                <li>
+                  <a href="{{ route('login') }}" class="me-2 btn btn-outline-light" role="button" aria-disabled="true">Login</a>
+                </li>  
+                <li>
+                  <a href="{{ route('register') }}" class="me-3 btn btn-outline-light" role="button" aria-disabled="true">Register</a>
+                </li>
+            @endguest  
 
-        </ul>    
-      </div>
-    </nav>
+          </ul>    
+        </div>
+      </nav>
 
+      @yield('content')
 
-    @yield('content')
-
+    </div>
   </body>
 </html>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,18 +5,12 @@
 @endsection
 
 @section('content')
-    <div id="bg-img">
-        <div class="h-100 container-fluid object-fit">
-            <div class="h-75 row d-flex justify-content-center align-items-center">
-                <div id="container" class="card text-white bg-dark border-light p-4 col-4">
-                    
-                    <div style="text-align: center">
-                        @yield('form')
-                    
-                    </div>
-                </div>
-            </div>
+    <div id="container" class="card position-absolute top-50 start-50 translate-middle text-white bg-dark border-light p-4 col-4">
+        
+        <div style="text-align: center">
+            @yield('form')
+        
         </div>
-    </div>    
+    </div>
 @endsection
 
