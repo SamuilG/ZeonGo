e33907dd4239bc033b4ae6d81bff35299f69770c
ilko27 <ilko.petrov27@gmail.com>
2021-11-12 20:38:20 +0200

    a

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pass;
-use App\Models\Device;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class DeviceController extends Controller
@@ -12,6 +11,14 @@ class DeviceController extends Controller
     public function process(Request $request)
     {
         $userKey = $request->userKey;
-        return $userKey;
+        $check = DB::table('user_keys')
+            ->where('user_key', $userKey)
+            ->join('passes', 'user_keys.user_id', '=' , 'passes.user_id')
+            ->where('device_id', 1)
+            ->get();
+        //dd($userKey);
+        return(count($check));
+
+        // SELECT * FROM `user_keys` JOIN passes ON user_keys.user_id = passes.user_id WHERE user_keys.user_key = 'pihNACDBlACvx4Z0Tcr5srxnA2EdlUDmW23Cgbtdk8HKYXjs9UszBafXs1wxzXzfnOkhh3T23Jhgt7Wyt2ZCBtC4gwDzK0ftJEhN' AND passes.device_id = 2
     }
 }
