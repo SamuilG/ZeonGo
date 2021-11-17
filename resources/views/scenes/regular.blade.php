541975df0b83bc93971ddd675a28076a8ee7cbc3
SamuilG <samuil.georgiev@outlook.com>
2021-11-17 19:55:04 +0200

    little bit of css

--- resources/views/scenes/regular.blade.php
+++ resources/views/scenes/regular.blade.php
@@ -1,20 +1,34 @@
 @extends('layouts.app')
 
+@section('title')
+    Home
+@endsection
+
+@section('headContent')
+    <link rel="stylesheet" href="/css/regular.css">
+@endsection
+
 @section('content')
-        <div class="bg-primary">
-            {{-- <h5>{{ session('key') }}</h5> --}}
-            {{-- <h5>{{ $key }}</h5> --}}
-            <img src="{{ $data['key'] }}" class="img-fluid">
+
+    <div id="content">
+        {{-- QR code --}}
+        <div class="w-50 float-start text-center">
+            
+            <img src="{{ $data['key'] }}" class="w-75 p-5">
         </div>
-        {{ $data['devices'] }}
+        {{-- Actions --}}
+        <div class="w-50 float-end">
+            <button disabled="disabled">test</button><br><br>
+            <button disabled="disabled">test</button><br><br>
+            <button disabled="disabled">test</button><br><br>
+            <button disabled="disabled">test</button><br><br>
+            
+        </div>
+    
         @foreach ($data['devices'] as $device)
-            {{$device->device_name}}
+            {{ $device }}
         @endforeach
-        <br>
-        {{ $data['history'] }}
-        @foreach ($data['history'] as $item)
-            {{$item->created_at}} {{-- get the date --}}
-            {{$item->device_name}} {{-- get the device name --}}
-        @endforeach
-    {{-- <img src="/images/meme.jpg"> --}}
+        {{-- <img src="/images/meme.jpg"> --}}
+    </div>
+    
 @endsection
