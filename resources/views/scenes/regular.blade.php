c8d8e9411dfd11668cb68d3ccb57951002cff414
SamuilG <samuil.georgiev@outlook.com>
2021-12-17 15:54:00 +0200

    just why

--- resources/views/scenes/regular.blade.php
+++ resources/views/scenes/regular.blade.php
@@ -7,7 +7,7 @@ crossorigin=""/>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
    crossorigin=""></script>
-<link rel="stylesheet" href="/css/regular.css">
+<link rel="stylesheet" href="/css/use.css">
 @endsection
 
 @section('title')
@@ -19,17 +19,18 @@ crossorigin=""/>
 <table id="regular" class="col-12">
     <tr>
         <td id="td_locations" class="col-8">
-            <div id="locations" class="col-7">
+            <div id="locations" class="col-8">
                 <br>
                 <div class="container-fluid overflow-auto">
                     <div class="row flex-nowrap">
 
                         @foreach ($data['devices'] as $device)
-                        <div class="col-4">
+                        <div class="col-3">
                             <div class="card h-100 card-block bg-dark">
                                 <div class="card-body">
-                                    <h5 class="card-title text-white">{{$device->device_name}}</h5>
+                                    <h4 class="card-title text-white pb-2">{{$device->device_name}}</h4>
                                     <p class="card-text text-white">
+                                        fglkdjkjkfdl dfnmgksdnfgkjsdn hgkjshdkjfn sdhgkjsdhm dksjhgtskdnvlk sfjkhgkjsldng 
                                         {{ $device->device_description }}
                                     </p>
                                     
@@ -74,14 +75,6 @@ crossorigin=""/>
         <td class="col-4" rowspan="2">
             
             <div id="history">
-                {{-- <table class="list-group">
-                    
-                        <tr class="list-group-item">
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    
-                </table> --}}
 
                 <h3 class="text-center">History</h3>
                 <ul class="list-group m-1">
@@ -90,19 +83,6 @@ crossorigin=""/>
                     @php
                         $i = 0
                     @endphp
-                  {{-- @foreach ($data['history'] as $item)
-                      <tr>
-                          <td class="w-50 pt-3 text-center">{{$item->device_name}}</td>
-                          <td class="w-50 pt-3 text-center">{{$item->created_at->diffForHumans()}}</td>
-                      </tr>
-                      @php
-                          $i++
-                      @endphp
-                      @if ($i > 15)
-                          @break
-                      @endif
-                  @endforeach
-                  <tr><td colspan="2" class="text-center"><a href="/hisory">View full history</a></td></tr> --}}
                     
                         @foreach ($data['history'] as $log)
                         <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -112,27 +92,12 @@ crossorigin=""/>
                         @php
                             $i++
                         @endphp
-                        @if ($i > 15)
+                        @if ($i > 10)
                             @break
                         @endif
                         @endforeach
                         <li class="list-group-item d-flex justify-content-between align-items-center"><a href="/history">View full history</a></li>
                         
-
-                
-                        {{-- @foreach ($data['history'] as $item)
-                            <tr>
-                                <td class="w-50 pt-3 text-center">{{$item->device_name}}</td>
-                                <td class="w-50 pt-3 text-center">{{$item->created_at->diffForHumans()}}</td>
-                            </tr>
-                            @php
-                                $i++
-                            @endphp
-                            @if ($i > 15)
-                                @break
-                            @endif
-                        @endforeach
-                        <tr><td colspan="2" class="text-center"><a href="/hisory">View full history</a></td></tr> --}}
                         
                 @else
                     <li class="list-group-item d-flex justify-content-between align-items-center">No history yet</li>
