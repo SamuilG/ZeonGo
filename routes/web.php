f46b142b7763ac2e94757b47a8cd7477f42ba072
ilko27 <ilko.petrov27@gmail.com>
2022-01-10 17:29:52 +0200

    a

--- routes/web.php
+++ routes/web.php
@@ -25,50 +25,50 @@ use App\Http\Controllers\Scene\RegularController;
 */
 
 // Route::get('/create', function(){
-//   User::create([
-//     'name' => 'Ilko',
-//     'email' => 'ilko.petrov27@gmail.com',
-//     'password' => Hash::make('123'),
-//     'email_verified' => '1'
-//   ]);
-//   Device::create([
-//     'device_name' => 'EG Geo Milev',
-//     'device_description' => 'EG Geo Milev is a school located in Dobrich. Sami the giga chad is from there!',
-//     'coordinates' => '43.5660385636653, 27.81893540981833',
-//     'device_key' => Device::createKey(),
-//   ]);
-//   Device::create([
-//     'device_name' => 'PMG Ivan Vazov',
-//     'device_description' => 'PMG Ivan Vazov is a school located in Dobrich. Ilko studies there!',
-//     'coordinates' => '43.564057234319115, 27.828727169323802',
-//     'device_key' => Device::createKey(),
-//   ]);
-//   Device::create([
-//     'device_name' => 'AAAAAAAAAA',
-//     'device_description' => 'AAAAAAAAAAAAAAAAA',
-//     'coordinates' => '43.57019220851931, 27.82741656421111',
-//     'device_key' => Device::createKey(),
-//   ]);
-//   Pass::create([
-//     'device_id' => 1,
-//     'user_id' => 1,
-//     'approved' => true,
-//   ]);
-//   Pass::create([
-//     'device_id' => 2,
-//     'user_id' => 1,
-//     'approved' => true,
-//   ]);
-//   Pass::create([
-//     'device_id' => 3,
-//     'user_id' => 1,
-//     'approved' => true,
-//   ]);
-//   Manager::create([
-//     'device_id' => 2,
-//     'user_id' => 1
-//   ]);
-//   History::factory()->times(100)->create();
+  // User::create([
+  //   'name' => 'Ilko',
+  //   'email' => 'ilko.petrov27@gmail.com',
+  //   'password' => Hash::make('123'),
+  //   'email_verified' => '1'
+  // ]);
+  // Device::create([
+  //   'device_name' => 'EG Geo Milev',
+  //   'device_description' => 'EG Geo Milev is a school located in Dobrich. Sami the giga chad is from there!',
+  //   'coordinates' => '43.5660385636653, 27.81893540981833',
+  //   'device_key' => Device::createKey(),
+  // ]);
+  // Device::create([
+  //   'device_name' => 'PMG Ivan Vazov',
+  //   'device_description' => 'PMG Ivan Vazov is a school located in Dobrich. Ilko studies there!',
+  //   'coordinates' => '43.564057234319115, 27.828727169323802',
+  //   'device_key' => Device::createKey(),
+  // ]);
+  // Device::create([
+  //   'device_name' => 'AAAAAAAAAA',
+  //   'device_description' => 'AAAAAAAAAAAAAAAAA',
+  //   'coordinates' => '43.57019220851931, 27.82741656421111',
+  //   'device_key' => Device::createKey(),
+  // ]);
+  // Pass::create([
+  //   'device_id' => 1,
+  //   'user_id' => 1,
+  //   'approved' => true,
+  // ]);
+  // Pass::create([
+  //   'device_id' => 2,
+  //   'user_id' => 1,
+  //   'approved' => true,
+  // ]);
+  // Pass::create([
+  //   'device_id' => 3,
+  //   'user_id' => 1,
+  //   'approved' => true,
+  // ]);
+  // Manager::create([
+  //   'device_id' => 2,
+  //   'user_id' => 1
+  // ]);
+  // History::factory()->times(100)->create();
 // });
 
 // Home
